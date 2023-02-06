6f0bfb9004a1c6da9d11cc03baec07427e23a423
luskoeqk <77803201+luskoeqk@users.noreply.github.com>
2023-02-06 22:14:08 +0200

    requests

--- express-app/routes/students.js
+++ express-app/routes/students.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 
+const Student = require('../models/student');
 
 // get all
-router.get('/', (req, res) => {
-    res.send('get all route')
+router.get('/', async (req, res) => {
+    try {
+        const students = await Student.find();
+        res.json(students);
+    } catch (err) {
+        res.status(500).jsong({ message: err.message })         // 500 error on the server 
+    }
 })
 
 // get one
@@ -14,8 +20,21 @@ router.get('/:id', (req, res) => {
 })
 
 // create one
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
 
+    const student = new Student({
+        name: req.body.name,
+        faculty_number: req.body.faculty_number,
+        email: req.body.email,
+        date_of_creation: req.body.date_of_creation
+    })
+
+    try {
+        const newStudent = await student.save();
+        res.status(201).json(newStudent);              // 201 successfully created an object
+    } catch (err) {
+        res.status(400).json({ message: err.message })                             // 400 bad data 
+    }
 })
 
 
