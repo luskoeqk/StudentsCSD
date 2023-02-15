ac76f28dd76895f71450007b02fd11cb1dab3444
luskoeqk <lusy2017@gmail.com>
2023-02-15 11:24:05 +0200

    getOne api route

--- next-app/src/pages/api/test/getOne.ts
+++ next-app/src/pages/api/test/getOne.ts
@@ -1,31 +1,30 @@
-import Test from 'models/testModel/testModel';
+import { NextApiRequest, NextApiResponse } from 'next';
 import connectMongoDB from 'utils/connectMongoDB';
+import Test from 'models/testModel/testModel';
 
-import type { NextApiRequest, NextApiResponse } from 'next'
 
+export default async function getOne(req: NextApiRequest, res: NextApiResponse) {
 
-export default async function updateTest(req: NextApiRequest, res: NextApiResponse) {
-
-    if (req.method === 'PATCH') {
+    if (req.method === 'GET') {
 
         try {
 
             connectMongoDB(); // Connect to the MongoDB database
-            const { _id, facultyNumber, name, email } = req.body;
 
-            const updatedUser = await Test.findByIdAndUpdate(_id, { facultyNumber, name, email }, { new: true });
+            const { id } = req.query;
+            const user = await Test.findById(id);
 
-            if (updatedUser) {
+            if (user) {
 
-                res.status(200).json({ message: 'User updated successfully', user: updatedUser });
+                res.status(200).json({ message: 'User found', user });
             } else {
 
-                res.status(404).json({ message: req.body });
+                res.status(404).json({ message: 'User not found' });
             }
         } catch (error) {
 
             console.error(error);
-            res.status(500).json({ message: 'Server error', error });
+            res.status(500).json({ message: 'Server error' });
         }
     } else {
 
