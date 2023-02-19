de86b828925c9302f44c6c0d6fd8c51d0ef23c32
luskoeqk <77803201+luskoeqk@users.noreply.github.com>
2023-02-19 21:17:45 +0200

    Update student.js

--- express-app/models/student.js
+++ express-app/models/student.js
@@ -4,19 +4,16 @@ const studentSchema = new mongoose.Schema({
 
     name: {
         type: String,
-        required: true
     },
     faculty_number: {
         type: String,
-        required: true
     },
     email: {
         type: String,
-        required: true
     },
+
     date_of_creation: {
         type: Date,
-        required: true,
         default: Date.now
     }
 });
